c3ec7812e6c40f46b83a021bb11b9924f4059271
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2026-05-29 13:13:15 +0530

    28

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,7 +76,7 @@ export default async function Home() {
                     {user?.name}
                   </h3>
                   <p className="text-sm text-slate-500 dark:text-slate-400 line-clamp-1">
-                    @{user?.username || user?.name?.split(' ')[0].toLowerCase()}
+                    @{user?.username || user?.name?.split(' ')?.[0]?.toLowerCase()}
                   </p>
                 </div>
               </div>
